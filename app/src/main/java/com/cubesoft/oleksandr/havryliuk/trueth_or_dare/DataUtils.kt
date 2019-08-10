fa2e54ba23da7e060cbaeca1fd6ef729ff50426e
sashaCube <sashagavgavgav@gmail.com>
2019-08-10 21:44:06 +0300

    create DataUtils

---
+++ app/src/main/java/com/cubesoft/oleksandr/havryliuk/trueth_or_dare/DataUtils.kt
@@ -0,0 +1,10 @@
+package com.cubesoft.oleksandr.havryliuk.trueth_or_dare
+
+const val CONTENT = "content"
+const val DOCUMENT_ID = "77ILQNof8dn9m8Qh551P"
+const val QUESTIONS = "questions"
+const val ACTIONS = "actions"
+const val TAG_CONTENT = "ContentRepository"
+const val STATE_LOADED = "loaded"
+const val STATE_EMPTY = "empty"
+const val PLAYERS = "players"
