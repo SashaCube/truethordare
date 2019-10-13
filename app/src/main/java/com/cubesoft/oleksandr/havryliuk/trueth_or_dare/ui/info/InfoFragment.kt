f6c63197590412810c5442e8f7116664cdb762e9
sashaCube <sashagavgavgav@gmail.com>
2019-10-13 17:52:01 +0300

    add: anko view to InfoFragment

--- app/src/main/java/com/cubesoft/oleksandr/havryliuk/trueth_or_dare/ui/info/InfoFragment.kt
+++ app/src/main/java/com/cubesoft/oleksandr/havryliuk/trueth_or_dare/ui/info/InfoFragment.kt
@@ -2,24 +2,37 @@ package com.cubesoft.oleksandr.havryliuk.trueth_or_dare.ui.info
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.findNavController
 import com.cubesoft.oleksandr.havryliuk.trueth_or_dare.R
-import kotlinx.android.synthetic.main.info_fragment.*
+import com.cubesoft.oleksandr.havryliuk.trueth_or_dare.ui.base.view.custom.*
+import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.relativeLayout
+import org.jetbrains.anko.support.v4.ctx
+import org.jetbrains.anko.verticalLayout
 
 class InfoFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        setHasOptionsMenu(true)
-        return inflater.inflate(R.layout.info_fragment, container, false)
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        info_back_btn_iv.setOnClickListener {
-            findNavController().popBackStack()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = with(AnkoContext.create(ctx, this)) {
+        relativeLayout {
+            verticalLayout {
+                relativeLayout {
+                    backButton {
+                        setOnClickListener {
+                            findNavController().popBackStack()
+                        }
+                    }
+                    titleTextView(R.string.about_game_label)
+                }
+                divider()
+                simpleTextView(R.string.game_rules)
+            }
+            linkTextView(R.string.privacy_policy)
         }
     }
 }
