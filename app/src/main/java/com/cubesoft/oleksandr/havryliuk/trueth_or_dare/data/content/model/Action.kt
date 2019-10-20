07e42cc96c68988474d6afc701c36320af0b0fde
sashaCube <sashagavgavgav@gmail.com>
2019-10-20 19:45:46 +0300

    add content models

---
+++ app/src/main/java/com/cubesoft/oleksandr/havryliuk/trueth_or_dare/data/content/model/Action.kt
@@ -0,0 +1,5 @@
+package com.cubesoft.oleksandr.havryliuk.trueth_or_dare.data.content.model
+
+data class Action(
+    override var text: String
+) : Content(text)
---
+++ app/src/main/java/com/cubesoft/oleksandr/havryliuk/trueth_or_dare/data/content/model/Content.kt
@@ -0,0 +1,5 @@
+package com.cubesoft.oleksandr.havryliuk.trueth_or_dare.data.content.model
+
+open class Content(
+    open var text: String
+)
